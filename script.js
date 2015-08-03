df0deb5b4829d5d1267b467bcbfb38bf7142692e
unknown <maicon.giovani@inepe.com.br>
2015-08-03 14:12:45 -0300

    final

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-$(document).ready();
+$(document).ready(function () {
+$('div').mouseenter(function () {
+$('div').fadeTo('fast', 1); 
+});
+$('div').mouseleave(function(){
+    $('div').fadeTo('fast', 0.5);
+    });
+});
